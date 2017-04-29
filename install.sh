c7759b53d2ab2452f48883bbd7cb5c6bbb9b4f97
Hieu Pham <dpham810@gmail.com>
2017-04-29 15:04:36 -0700

    install tmux.conf

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ echo $current_path
 mkdir -p vim/bundle
 rm -rf vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git vim/bundle/Vundle.vim
+
 ln -s $current_path/vimrc ~/.vimrc
 ln -s $current_path/vimrc.local ~/.vimrc.local
 ln -s $current_path/vimrc.bundles ~/.vimrc.bundles
 ln -s $current_path/vim ~/.vim
+ln -s $current_path/tmux.conf ~/.tmux.conf
 touch ~/.vimrc.bundles.local
 
 vim +PluginInstall +qall
