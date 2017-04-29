5e9f3d8c58ec74b86cf7dc50af6daf7e06583d6c
Hieu Pham <dpham810@gmail.com>
2017-04-29 09:22:05 -0700

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+current_path=$(pwd)
+echo $current_path
+
 mkdir -p vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git vim/bundle/Vundle.vim
-
-ln -s vimrc ~/.vimrc
-ln -s vimrc.local ~/.vimrc.local
-ln -s vimrc.bundles ~/.vimrc.bundles
-ln -s vim ~/.vim
+ln -s $current_path/vimrc ~/.vimrc
+ln -s $current_path/vimrc.local ~/.vimrc.local
+ln -s $current_path/vimrc.bundles ~/.vimrc.bundles
+ln -s $current_path/vim ~/.vim
 
 vim +PluginInstall +qall
