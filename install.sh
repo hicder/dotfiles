735e9c91100f71a4eb043e88b486e86e197b5504
Hieu Pham <dpham810@gmail.com>
2017-05-01 17:14:40 -0700

    update

--- install.sh
+++ install.sh
@@ -5,6 +5,13 @@ mkdir -p vim/bundle
 rm -rf vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git vim/bundle/Vundle.vim
 
+echo "Delete existing dotfiles..."
+rm ~/.vimrc
+rm ~/.vimrc.local
+rm ~/.vimrc.bundles
+rm ~/.tmux.conf
+
+echo "Installing new dotfiles..."
 ln -s $current_path/vimrc ~/.vimrc
 ln -s $current_path/vimrc.local ~/.vimrc.local
 ln -s $current_path/vimrc.bundles ~/.vimrc.bundles
